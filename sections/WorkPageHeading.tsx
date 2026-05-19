ca649e65799ad0fcad669d8394b06647f243f5fc
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-05-19 16:40:33 +0530

    working on work page

--- sections/WorkPageHeading.tsx
+++ sections/WorkPageHeading.tsx
@@ -1,9 +1,52 @@
+"use client";
+
 import PlanetHorizon from "@/components/PlanetHorizon";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const WorkPageHeading = () => {
+  const headingContainerRef = useRef(null);
+  const headingRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: headingRef.current,
+        markers: true,
+        start: "top top+=250px",
+        end: "bottom top+=250px",
+        scrub: 1,
+        //   pin: true,
+      },
+    });
+    //   .to(headingRef.current, {
+    //     top: "15%",
+    //     scale: 0.4,
+    //   });
+    // gsap.to(headingRef.current, {
+    //   scrollTrigger: {
+    //     trigger: headingRef.current,
+    //     markers: true,
+    //     start: "top top+=250px",
+    //     end: "bottom top+=250px",
+    //     scrub: 1,
+    //     pin: true,
+    //   },
+    //   top: "15%",
+    //   scale: 0.4,
+    // });
+  }, []);
+
   return (
-    <>
-      <h1 className="font-semibold text-[180px] relative top-[25%] text-center select-none z-5">
+    <div ref={headingContainerRef} className="h-150 flex flex-col justify-end">
+      <h1
+        ref={headingRef}
+        className="font-semibold text-[180px] relative text-center select-none z-5"
+      >
         {"SHOWCASE".split("").map((letter, index) => (
           <span
             key={index}
@@ -18,7 +61,7 @@ const WorkPageHeading = () => {
         ))}
       </h1>
       <PlanetHorizon />
-    </>
+    </div>
   );
 };
 
