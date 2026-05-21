9524d9a0491fd4ea0d03a4285634df9725e4f07e
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-05-21 16:44:52 +0530

    adding cta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
+import CTA from "@/components/CTA";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </SmoothScroll>
+        <CTA />
       </body>
     </html>
   );
---
+++ components/CTA.tsx
@@ -0,0 +1,11 @@
+const CTA = () => {
+  return (
+    <div className="fixed inset-x-0 bottom-15">
+      <div className="py-5 text-center bg-background/20 backdrop-blur-sm border border-white/40 w-4/5 mx-auto">
+        <span>accept cookies policy banner goes here!</span>
+      </div>
+    </div>
+  );
+};
+
+export default CTA;
