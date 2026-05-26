805945a664583231a6f51c6607d888e254403bed
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-05-26 15:04:54 +0530

    minor fix

--- sections/HomeReel.tsx
+++ sections/HomeReel.tsx
@@ -4,10 +4,10 @@ import showreel from "@/videos/showreel.mp4";
 const HomeReel = () => {
   return (
     <div className="flex justify-center items-center h-full relative">
-      <div className="bg-white container-clip-both">
+      <div className="bg-white 2xl:w-3/5 max-md:w-9/10 max-sm:max-w-120 sm:max-md:max-w-180 2xl:max-w-400 container-clip-both">
         <Video
           src={showreel}
-          className="2xl:w-3/5 2xl:max-w-400 max-md:w-4/5 max-sm:max-w-90 sm:max-md:max-w-180 video-container p-0.5 shadow-[0_0_10px_8px_rgba(255,173,64,0.1)] content-clip-both"
+          className="video-container p-0.5 shadow-[0_0_10px_8px_rgba(255,173,64,0.1)] content-clip-both"
           controls={false}
           autoPlay
           loop
