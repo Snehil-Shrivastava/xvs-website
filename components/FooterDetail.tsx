300c4bec2beed92c50e066c7471b8f43198c6219
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-06-03 14:23:51 +0530

    fixing footer responsiveness

--- components/FooterDetail.tsx
+++ components/FooterDetail.tsx
@@ -8,7 +8,7 @@ const FooterDetail = () => {
   const footerServices = FooterServices;
   return (
     <div className="relative">
-      <div className="py-15 max-md:pt-3 max-md:pb-5 w-[85%] max-sm:w-9/10 md:max-lg:w-9/10 lg:max-xl:w-[95%] xl:max-1440p:w-9/10 mx-auto flex gap-70 md:max-lg:gap-15 lg:max-xl:gap-20 xl:max-1440p:gap-25 1440p:max-2240p:gap-30 relative z-5">
+      <div className="py-15 max-md:pt-3 max-md:pb-5 w-[85%] max-sm:w-full sm:max-md:w-full md:max-lg:w-9/10 lg:max-xl:w-[95%] xl:max-1440p:w-9/10 mx-auto flex md:max-lg:gap-15 lg:max-xl:gap-20 xl:max-2xl:gap-25 2xl:max-1920p:gap-20 1920p:max-2240p:gap-25 2240p:gap-35 relative z-5">
         <div className="flex flex-col justify-between gap-10 max-md:hidden">
           <Link href={`/`}>
             <Image
@@ -30,8 +30,9 @@ const FooterDetail = () => {
           </div>
         </div>
         <div className="flex-1 relative font-poppins">
-          <div className="flex justify-between w-full max-sm:w-full sm:max-md:w-9/10 mx-auto h-full max-sm:gap-5 md:max-lg:gap-8">
-            <div className="flex flex-col gap-8 max-sm:gap-4 sm:max-md:gap-6 max-2240p:flex-[0.55]">
+          <div className="flex justify-between w-full max-md:w-full mx-auto h-full max-sm:gap-5 sm:max-md:gap-6 md:max-lg:gap-8 relative">
+            <div className="absolute w-[120%] bg-[#f7983944] h-px top-12 lg:max-xl:top-9.5 sm:max-lg:top-7.5 max-sm:top-7" />
+            <div className="flex flex-col gap-8 max-sm:gap-4 sm:max-md:gap-6 max-2240p:flex-[0.55] sm:max-md:pl-10 max-sm:pl-8">
               <span className="font-semibold text-[26px] max-sm:text-sm sm:max-md:text-lg md:max-lg:text-sm lg:max-xl:text-xl">
                 Contact
               </span>
@@ -43,7 +44,8 @@ const FooterDetail = () => {
                       Uttar Pradesh, India
                     </span>
                   </div>
-                  <div className="flex flex-col text-xl max-sm:text-[10px] sm:max-lg:text-sm lg:max-xl:text-lg">
+                  <div className="flex flex-col text-xl max-sm:text-[10px] sm:max-lg:text-sm lg:max-xl:text-lg relative">
+                    <div className="absolute w-px h-[140%] bg-[#f7983944] -left-10 max-sm:-left-5 -top-5.5 max-sm:-top-2" />
                     <span className="font-bold">Sheridan,</span>
                     <span className="text-brand-cream text-base max-sm:text-[8px] sm:max-lg:text-[10px] lg:max-xl:text-sm">
                       Wyoming, US
@@ -71,7 +73,8 @@ const FooterDetail = () => {
               <span className="font-semibold text-[26px] max-sm:text-sm sm:max-md:text-lg md:max-lg:text-sm lg:max-xl:text-xl">
                 Services
               </span>
-              <div className="grid grid-cols-2 gap-x-15 md:max-lg:gap-x-8 gap-y-2 max-md:flex max-md:flex-col max-md:gap-2 text-brand-cream">
+              <div className="grid grid-cols-2 gap-x-15 md:max-lg:gap-x-8 gap-y-2 max-md:flex max-md:flex-col max-md:gap-2 text-brand-cream relative">
+                <div className="absolute h-[120%] max-sm:h-[105%] bg-[#f7983944] w-px -left-20 max-lg:-left-10 max-md:-left-5 max-sm:-top-2 -top-5.5" />
                 {footerServices.map((service, index) => (
                   <Link
                     key={index}
@@ -107,9 +110,8 @@ const FooterDetail = () => {
       </div>
 
       {/* dividing lines */}
-      <div className="absolute h-px w-[75%] max-md:w-full md:max-lg:w-[78%] lg:max-xl:w-4/5 xl:max-1440p:w-[78%] bg-[#f7983944] top-[28%] max-sm:top-[12%] sm:max-md:top-[15%] lg:max-xl:top-[28%] right-0" />
-      <div className="absolute h-1/2 max-md:h-3/5 w-px bg-[#f7983944] right-[40%] max-sm:right-[40%] sm:max-md:right-[45%] top-[28%] max-sm:top-[12%] sm:max-md:top-[15%] lg:max-xl:top-[28%]" />
-      <div className="absolute h-[18%] max-md:h-[16%] w-px bg-[#f7983944] right-[65.5%] max-sm:right-[69%] sm:max-md:right-[73%] md:max-lg:right-[58%] lg:max-xl:right-[60%] xl:max-2xl:right-[57%] 2xl:max-2240p:right-[59%] top-[28%] max-sm:top-[12%] sm:max-md:top-[15%] lg:max-xl:top-[28%]" />
+      {/* <div className="absolute h-px w-[75%] max-md:w-full md:max-lg:w-[78%] lg:max-xl:w-4/5 xl:max-1440p:w-[78%] 2xl:max-1920p:w-[78%] 1920p:max-2240p:w-4/5 bg-[#f7983944] top-[28%] max-sm:top-[12%] sm:max-md:top-[15%] lg:max-xl:top-[28%] right-0" />
+      <div className="absolute h-1/2 max-md:h-3/5 w-px bg-[#f7983944] right-[40%] max-sm:right-[40%] sm:max-md:right-[45%] top-[28%] max-sm:top-[12%] sm:max-md:top-[15%] lg:max-xl:top-[28%]" /> */}
     </div>
   );
 };
