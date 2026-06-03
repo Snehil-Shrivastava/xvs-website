3cde674eb4281d6d70daf94b8a2afea5d8c7f414
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-06-03 16:53:43 +0530

    style changes

--- sections/BlogMain.tsx
+++ sections/BlogMain.tsx
@@ -11,14 +11,14 @@ const BlogMain = ({ activeCategory, activeTag }: BlogMainProps) => {
   const isFiltered = !!activeCategory || !!activeTag;
   return (
     <div className="flex gap-20 max-lg:gap-10">
-      <div className="flex-[0.7] font-poppins">
+      <div className="sm:flex-[0.7] font-poppins">
         {isFiltered ? (
           <BlogGrid activeCategory={activeCategory} activeTag={activeTag} />
         ) : (
           <FeaturedBlog />
         )}
       </div>
-      <div className="flex-[0.3]">
+      <div className="flex-[0.3] max-sm:hidden">
         <BlogSidebar activeCategory={activeCategory} activeTag={activeTag} />
       </div>
     </div>
