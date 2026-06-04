965967d563bb36d88d4f452e2ab87bb78fe89c11
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-06-04 15:00:38 +0530

    attempt 2

--- app/(frontend)/(app)/blogs/page.tsx
+++ app/(frontend)/(app)/blogs/page.tsx
@@ -1,6 +1,8 @@
+import BlogPageSkeleton from "@/components/BlogPageSkeleton";
 import BlogsPage from "@/page/BlogsPage";
 import blogBg from "@/videos/blog.mp4";
 import Video from "next-video";
+import { Suspense } from "react";
 
 interface PageProps {
   searchParams: Promise<{ category?: string; tag?: string }>;
@@ -8,27 +10,30 @@ interface PageProps {
 
 const Blogs = async ({ searchParams }: PageProps) => {
   return (
-    <div>
-      <div
-        className="h-auto max-sm:h-150 sm:max-lg:h-220 absolute z-0 inset-x-0 overflow-hidden brightness-50"
-        style={{
-          maskImage: "linear-gradient(to bottom, black, transparent 95% 100%)",
-        }}
-      >
-        <div className="overflow-hidden max-sm:w-[250vw] sm:max-md:w-[180vw] md:max-xl:w-[200vw] w-[200vw]">
-          <Video
-            src={blogBg}
-            className="-scale-x-100 services-bg-container h-[90vh] max-xl:h-full"
-            controls={false}
-            autoPlay
-            loop
-            muted
-            playsInline
-          />
+    <Suspense fallback={<BlogPageSkeleton />}>
+      <div>
+        <div
+          className="h-auto max-sm:h-150 sm:max-lg:h-220 absolute z-0 inset-x-0 overflow-hidden brightness-50"
+          style={{
+            maskImage:
+              "linear-gradient(to bottom, black, transparent 95% 100%)",
+          }}
+        >
+          <div className="overflow-hidden max-sm:w-[250vw] sm:max-md:w-[180vw] md:max-xl:w-[200vw] w-[200vw]">
+            <Video
+              src={blogBg}
+              className="-scale-x-100 services-bg-container h-[90vh] max-xl:h-full"
+              controls={false}
+              autoPlay
+              loop
+              muted
+              playsInline
+            />
+          </div>
         </div>
+        <BlogsPage searchParams={searchParams} />
       </div>
-      <BlogsPage searchParams={searchParams} />
-    </div>
+    </Suspense>
   );
 };
 
