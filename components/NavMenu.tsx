c9b857dd7ca78c7b48a4c2236a83a5317689cef7
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-06-08 11:42:25 +0530

    fix: nav menu content overflowing in y direction

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -14,7 +14,7 @@ const NavMenu = ({ closeMenu }: { closeMenu: () => void }) => {
   const navLinksBody = NavMenuLinksBody;
   return (
     <div className="absolute inset-0 flex flex-col">
-      <div className="px-50 pt-20 pb-15 max-sm:pt-25 sm:max-md:pt-30 md:max-lg:pt-40 lg:max-xl:pt-45 max-sm:px-10 sm:max-md:px-35 md:max-xl:px-0 max-sm:pb-20">
+      <div className="px-50 pt-20 pb-15 max-sm:pt-25 sm:max-md:pt-30 md:max-lg:pt-40 lg:max-xl:pt-45 xl:max-2xl:pt-40 2xl:max-1920p:pt-45 1920p:pt-40 xl:max-2xl:px-0 max-sm:px-10 sm:max-md:px-35 md:max-xl:px-0 max-sm:pb-10">
         <div className="font-poppins flex items-center justify-center sm:max-md:justify-between max-sm:gap-3 sm:max-md:gap-6 md:max-lg:gap-15 gap-20 max-sm:text-[1.125rem] sm:max-lg:text-2xl lg:max-xl:text-3xl text-[2.3rem] font-extralight max-sm:justify-between max-sm:mx-auto">
           {mainNavLinks.map((link, index) => (
             <Link
@@ -29,8 +29,8 @@ const NavMenu = ({ closeMenu }: { closeMenu: () => void }) => {
           ))}
         </div>
       </div>
-      <div className="flex justify-center max-xs:h-105 max-xs:overflow-y-scroll sm:max-md:h-120 md:max-lg:h-180 lg:max-xl:h-200 sm:max-xl:overflow-y-scroll max-lg:w-[70%] max-lg:justify-start text-brand-orange py-20 max-xl:py-0 mx-auto">
-        <div className="max-md:w-full xl:max-1440p:w-[65%] 1440p:max-2xl:w-[85%] 2xl:max-2240p:w-[65%] 2240p:w-[70%] max-xl:flex max-xl:flex-col grid grid-cols-2 max-sm:gap-[2dvh] sm:max-md:gap-[3dvh] md:max-lg:gap-[3dvh] lg:max-xl:gap-[4dvh] xl:max-1440p:gap-[7dvh] 1440p:max-2xl:gap-x-[7dvh] 1440p:max-2xl:gap-y-[12dvh] 2xl:gap-[8dvh] 2240p:gap-y-[7dvh] 2240p:gap-x-[9dvh]">
+      <div className="flex justify-center max-xs:h-105 max-xs:overflow-y-scroll sm:max-md:h-120 md:max-lg:h-180 lg:max-xl:h-120 xl:max-1440p:h-150 1440p:max-2xl:h-180 2xl:max-1920p:h-150 1920p:h-180 overflow-y-scroll max-lg:w-[70%] lg:max-xl:w-4/5 xl:max-1440p:w-[70%] max-lg:justify-start text-brand-orange py-0 mx-auto 1440p:max-2xl:px-50">
+        <div className="max-md:w-full xl:max-1440p:w-full 1440p:max-2xl:w-full 2xl:max-2240p:w-[65%] 2240p:w-[70%] max-lg:flex max-lg:flex-col grid grid-cols-2 max-sm:gap-[2dvh] sm:max-md:gap-[3dvh] md:max-lg:gap-[3dvh] lg:max-xl:gap-[4dvh] xl:max-1440p:gap-[7dvh] 1440p:max-2xl:gap-x-[7dvh] 1440p:max-2xl:gap-y-0 2xl:gap-[8dvh] 2240p:gap-y-[7dvh] 2240p:gap-x-[9dvh]">
           {navLinksBody.map((link, index) => (
             <div key={index} className="">
               <h3 className="font-poppins max-sm:text-[10px] sm:max-md:text-[0.75rem] md:max-lg:text-[0.875rem] lg:max-xl:text-[1rem] xl:max-1440p:text-[1.2rem] 1440p:max-2xl:text-[1.2rem] 2xl:text-[1.5rem] 2240p:text-[30px]">
@@ -41,7 +41,7 @@ const NavMenu = ({ closeMenu }: { closeMenu: () => void }) => {
                   pathname: "/work",
                   query: { category: link.heading },
                 }}
-                className={`block font-calSans max-sm:text-[36px]/[48px] sm:max-md:text-[1.5rem]/[2rem] md:max-lg:text-[1.8rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-1440p:text-[3rem]/[3.75rem] 1440p:max-2xl:text-[4.5rem]/[4.3rem] 2xl:text-[3.5rem]/[4rem] 2240p:text-[82px]/[100px] 2240p:w-[90%]`}
+                className={`block font-calSans max-sm:text-[36px]/[48px] sm:max-md:text-[1.5rem]/[2rem] md:max-lg:text-[1.8rem]/[2.5rem] lg:max-xl:text-[2.2rem]/[2.8rem] xl:max-1440p:text-[3rem]/[3.75rem] 1440p:max-2xl:text-[4rem]/[3.7rem] 2xl:text-[3.5rem]/[4rem] 2240p:text-[82px]/[100px] 2240p:w-[90%]`}
                 onClick={closeMenu}
               >
                 {link.heading}
@@ -50,49 +50,51 @@ const NavMenu = ({ closeMenu }: { closeMenu: () => void }) => {
           ))}
         </div>
       </div>
-      <div className="flex justify-end max-md:justify-center gap-10 px-30 max-sm:px-0 mt-auto mb-25 max-sm:mb-10 max-xl:mt-10">
-        <Link
-          href="https://www.behance.net/xVSCreations"
-          target="_blank"
-          className="max-sm:w-4 sm:max-md:w-8 md:max-lg:w-5 lg:max-xl:w-7 xl:max-1440p:w-10 2240p:w-8 2240p:h-8"
-        >
-          <Image src={behance} alt="behance" className="" />
-        </Link>
-        <Link
-          href="https://www.instagram.com/xvscreations"
-          target="_blank"
-          className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
-        >
-          <Image src={instagram} alt="instagram" className="" />
-        </Link>
-        <Link
-          href="https://www.dribbble.com/xvscreations"
-          target="_blank"
-          className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
-        >
-          <Image src={dribble} alt="dribble" className="" />
-        </Link>
-        <Link
-          href="https://www.facebook.com/xVSCreations"
-          target="_blank"
-          className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
-        >
-          <Image src={facebook} alt="facebook" className="" />
-        </Link>
-        <Link
-          href="https://www.linkedin.com/company/xvs-creations"
-          target="_blank"
-          className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
-        >
-          <Image src={linkedin} alt="linkedin" className="" />
-        </Link>
-        <Link
-          href="https://x.com/xvscreations"
-          target="_blank"
-          className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
-        >
-          <Image src={twitter} alt="twitter" className="" />
-        </Link>
+      <div className="flex flex-1 items-end justify-end max-md:justify-center px-30 max-sm:px-0 mb-20 max-md:mb-10 md:max-2xl:mb-15 mt-10">
+        <div className="flex justify-end gap-10">
+          <Link
+            href="https://www.behance.net/xVSCreations"
+            target="_blank"
+            className="max-sm:w-4 sm:max-md:w-8 md:max-lg:w-5 lg:max-xl:w-7 xl:max-1440p:w-10 2240p:w-8 2240p:h-8"
+          >
+            <Image src={behance} alt="behance" className="" />
+          </Link>
+          <Link
+            href="https://www.instagram.com/xvscreations"
+            target="_blank"
+            className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
+          >
+            <Image src={instagram} alt="instagram" className="" />
+          </Link>
+          <Link
+            href="https://www.dribbble.com/xvscreations"
+            target="_blank"
+            className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
+          >
+            <Image src={dribble} alt="dribble" className="" />
+          </Link>
+          <Link
+            href="https://www.facebook.com/xVSCreations"
+            target="_blank"
+            className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
+          >
+            <Image src={facebook} alt="facebook" className="" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/company/xvs-creations"
+            target="_blank"
+            className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
+          >
+            <Image src={linkedin} alt="linkedin" className="" />
+          </Link>
+          <Link
+            href="https://x.com/xvscreations"
+            target="_blank"
+            className="max-sm:w-3.5 sm:max-md:w-6 md:max-lg:w-4 lg:max-xl:w-5 xl:max-1440p:w-7.5 2240p:w-6 2240p:h-8"
+          >
+            <Image src={twitter} alt="twitter" className="" />
+          </Link>
+        </div>
       </div>
     </div>
   );
