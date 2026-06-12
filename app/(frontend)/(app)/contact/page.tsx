703757ba29a73ebca43c28cc4510abfc936ddb53
sanjeevxvs <sanjeev@xvscreations.com>
2026-06-12 14:47:30 +0530

    fixing scroll issue on contact page

--- app/(frontend)/(app)/contact/page.tsx
+++ app/(frontend)/(app)/contact/page.tsx
@@ -5,9 +5,9 @@ import ContactPage from "@/page/ContactPage";
 
 const Contact = () => {
   useEffect(() => {
-    window.scrollTo(0, 0);
+    const t = setTimeout(() => window.scrollTo(0, 0), 0);
+    return () => clearTimeout(t);
   }, []);
-
   return (
     <div className="h-350 max-md:h-280 md:max-lg:h-260 lg:max-1440p:h-320">
       <ContactPage />
