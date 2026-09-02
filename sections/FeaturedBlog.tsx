901573c3bff004987298d9bb09c4a8960122f543
sanjeevxvs <sanjeev@xvscreations.com>
2026-09-02 14:23:33 +0530

    tag container does not render when no tags are present

--- sections/FeaturedBlog.tsx
+++ sections/FeaturedBlog.tsx
@@ -43,7 +43,7 @@ const FeaturedBlog = ({ posts }: { posts: PostType[] }) => {
               <h2 className="font-calSans 1920p:text-3xl xl:text-2xl lg:text-xl max-lg:text-lg flex-[0.65]">
                 {post.title}
               </h2>
-              {post.tags && (
+              {post.tags?.length !== 0 && (
                 <div className="flex-[0.35] flex gap-5 gap-y-0.5 flex-wrap text-sm max-xl:text-[12px] text-neutral-400">
                   {post.tags?.map((tag, i) => (
                     <span key={i}>#{tag.tag}</span>
